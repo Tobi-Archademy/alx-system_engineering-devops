b98c81e37f99f08fd646ef11862106527d7d1dc4
Oluwatobi Abass <oluwatobiabass5@gmail.com>
2022-01-03 05:04:16 -0800

    updated file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Creating a file in the tmp folder
+# creates a file in the /tmp directory
 file { '/tmp/school':
-  ensure   => file,
-  path     => '/tmp/school',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet'
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
